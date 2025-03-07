d6919be6e00578d33ae73b4e94d5529e007fb434
Admin <mramazanov@outlook.com>
2025-03-07 12:59:41 +0300

    Исправлена ошибка в сериализации redis cache

--- src/main/java/ru/jabka/x6_user/configuration/RedisConfiguration.java
+++ src/main/java/ru/jabka/x6_user/configuration/RedisConfiguration.java
@@ -42,7 +42,7 @@ public class RedisConfiguration {
                         )
                 )
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
-                        new Jackson2JsonRedisSerializer<>(cacheObjectMapper, UserResponse.class)
+                        new Jackson2JsonRedisSerializer<>(cacheObjectMapper, Boolean.class)
                 ))
                 .disableCachingNullValues();
 
